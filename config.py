97ac18b7e3a65c2cc9a226863ce9462f92d094cd
Michael <michael@adl-crm.uk>
2019-06-11 15:10:45 +0100

    Completed section 7: Error Handling: Lectures 46 - 52

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ class Config(object):
                                                           'app.db') + '?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['michael@adl-crm.uk']
