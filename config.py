29ae042f9712ead4bf5fee9c06af3f9286250154
Michael <michael@adl-crm.uk>
2019-07-10 15:45:24 +0100

    Completed Section 9: Pagination: Lectures 62 - 68

--- config.py
+++ config.py
@@ -15,3 +15,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['michael@adl-crm.uk']
+    POSTS_PER_PAGE = 3
