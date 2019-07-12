023a596e4dc556d2e2d520b92dab9c50abffa37c
Michael <michael@adl-crm.uk>
2019-07-12 12:04:20 +0100

    Completed Section 13: I18n and L10n: Lectures 91 - 99

--- config.py
+++ config.py
@@ -15,4 +15,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['michael@adl-crm.uk']
+    LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 3
